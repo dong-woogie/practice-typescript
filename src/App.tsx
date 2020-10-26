cc030595c59199c7660af067cd46594c715fba54
dong-woogie <com6511@gmail.com>
2020-10-27 01:40:56 +0900

    load image

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import GithubProfileLoader from "./containers/GithubProfileLoader";
 // import CounterContainer from "./containers/CounterContainer";
 // import TodoApp from "./containers/TodoApp";
+import Logo from "./static/logo.png";
 
 function App() {
   return (
     <div>
       <GithubProfileLoader />
+      <img src={Logo} alt={"logo"} />
     </div>
   );
 }
